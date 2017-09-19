2b97806d9efdd940e0d22e2dc5d5adc53beb9252
lvzhi <lvzhijim@163.com>
2017-09-19 21:42:13 +0800

    init respository

---
+++ blog/views.py
@@ -0,0 +1,55 @@
+#coding:utf-8
+import markdown
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post, Category
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    for post in post_list:
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                                  'markdown.extensions.extra',
+                                                  'markdown.extensions.codehilite',
+                                                  'markdown.extensions.toc',
+                                                  ])
+    for post in post_list:
+        post.excerpt = markdown.markdown(post.body,
+                                         extensions=[
+                                                     'markdown.extensions.extra',
+                                                     'markdown.extensions.codehilite',
+                                                     'markdown.extensions.toc',
+                                                     ])
+        form = CommentForm()
+        comment_list = post.comment_set.all()
+        context = {'post':post,
+                    'form':form,
+                    'comment_list':comment_list
+                  }
+    return render(request, 'blog/index.html', context=context)
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                              'markdown.extensions.extra',
+                                              'markdown.extensions.codehilite',
+                                              'markdown.extensions.toc',
+                                              ])
+    return render(request,'blog/detail.html', context={'post':post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
+
+
+
+
+
